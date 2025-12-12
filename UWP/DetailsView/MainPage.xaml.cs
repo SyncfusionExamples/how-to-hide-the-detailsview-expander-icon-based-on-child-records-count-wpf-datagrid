d47f6849e45fe3032319f64d0266f761a2d41770
SreemonPremkumarM <sreemonpremkumar.m@syncfusion.com>
2025-12-12 12:58:21 +0530

    Update MainPage.xaml.cs

--- UWP/DetailsView/MainPage.xaml.cs
+++ UWP/DetailsView/MainPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace DetailsView
 {
     /// <summary>
